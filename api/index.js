0d7c67fec4fda6e9e63e6d26f463752a0b57c256
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 11:26:35 -0300

    chore: update shareCode to Q0TTN35632R

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=5JFM6PGF26E&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=Q0TTN35632R&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
