d7d9f334a336593f47be59bb6de9c8c53c4d819c
JCPrime01 <vanzokiller@gmail.com>
2026-06-12 12:05:35 -0300

    chore: shareCode Y667R6V5LSY -> O5TKPYC8OBR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=Y667R6V5LSY&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=O5TKPYC8OBR&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
