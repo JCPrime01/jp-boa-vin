4fd131663638dd891cc6169d8b7613e4b5b98b1b
JCPrime01 <vanzokiller@gmail.com>
2026-06-12 20:00:22 -0300

    chore: shareCode O5TKPYC8OBR -> AX5FSHMVS92 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=O5TKPYC8OBR&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=AX5FSHMVS92&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
