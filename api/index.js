a3f268e6820c940c67eb92effd8d2b038e53acdf
JCPrime01 <vanzokiller@gmail.com>
2026-06-16 14:02:37 -0300

    chore: shareCode 1O1446GFDUV -> CR7YGMC4510 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=1O1446GFDUV&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=CR7YGMC4510&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
