0895213171b3822d7ff54cdfdb028eaea71a1f33
JCPrime01 <vanzokiller@gmail.com>
2026-06-18 19:03:38 -0300

    chore: shareCode NI6W88L0WF9 -> 3YI0W8TW8X6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=NI6W88L0WF9&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=3YI0W8TW8X6&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
