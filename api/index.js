a79531780ccb07e17974a4f3f8ee5b1053b77d93
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 10:13:05 -0300

    chore: shareCode V47F2FCZSTU -> ZP7UZMK9I4M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=V47F2FCZSTU&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=ZP7UZMK9I4M&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
