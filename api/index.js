1e96dcc58d4debabeffb344664119ecf04288528
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 12:11:35 -0300

    chore: shareCode 33FELV5BH6W -> 1W1Z81S4WEQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=33FELV5BH6W&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=1W1Z81S4WEQ&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
