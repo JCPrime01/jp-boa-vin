9819382b6cea65dd7c938616d0880ed3f696cdb6
JCPrime01 <vanzokiller@gmail.com>
2026-07-25 11:08:36 -0300

    chore: shareCode B84F6B52KQX -> C61M68PIUXE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=B84F6B52KQX&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=C61M68PIUXE&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
