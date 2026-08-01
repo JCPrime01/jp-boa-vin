6b242384b925fb518d3b0dc8490580c8aacd2386
JCPrime01 <vanzokiller@gmail.com>
2026-08-01 11:12:43 -0300

    chore: shareCode ML9BDAMJBHC -> WO4S52O4PUI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=ML9BDAMJBHC&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=WO4S52O4PUI&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
