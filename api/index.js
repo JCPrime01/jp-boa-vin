68b44a7599b03c6dfa9d97ba97b0501550982e72
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 12:55:51 -0300

    chore: shareCode F6AFROI4DGV -> UNK36DI9CVE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=F6AFROI4DGV&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=UNK36DI9CVE&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
