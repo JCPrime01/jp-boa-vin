7eb764634b81f3ccddd5d80b1c05f96897e6aa5f
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 13:45:50 -0300

    chore: shareCode DF50Z59PF8G -> VOA6GTMNQ81 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=DF50Z59PF8G&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=VOA6GTMNQ81&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
